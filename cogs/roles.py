a8d85b429e12f6746965a2c81bf60417d1923442
Arisen <pawelherzyk2002@gmail.com>
2024-05-25 13:53:02 +0200

    Added more roles

--- cogs/roles.py
+++ cogs/roles.py
@@ -7,7 +7,8 @@ class Roles(commands.Cog):
         bot.tree.add_command(self.role_modify,guild=discord.Object(id=692802312720089108))
         # dict construction "emoji_name":[emoji_id, role_id]
         self.role_info = {"squad":[1243605677063274538,1243594369009713163],"arma3":[1243606971488403590,903684702500700160],"COH2":[1243840205128073286,1243837274349899816],"GTFO":[1243840203869651025,1243837477215670302],\
-            "HD2":[1243840208936374354,1243836841720025228],"Ready_or_Not":[1243840201135095839,1243837361889218570],"cs2":[1243840206722044014,1243837029872308224],"dayz":[1243840207892119572,1243837200299200542],"rocket_league":[1243840202489991168,1243837073895460929]}
+            "HD2":[1243840208936374354,1243836841720025228],"Ready_or_Not":[1243840201135095839,1243837361889218570],"cs2":[1243840206722044014,1243837029872308224],"dayz":[1243840207892119572,1243837200299200542],"rocket_league":[1243840202489991168,1243837073895460929],\
+            "minecraft":[1243876361941160026,1243884155423686709],"JAVR_SMP":[1243883118826622997,1243884187195412531]}
         
 
     @discord.app_commands.command(name="role", description="Sends role selection message")
@@ -20,7 +21,9 @@ class Roles(commands.Cog):
             <:Ready_or_Not:1243840201135095839>  - Ready or Not \n\
             <:cs2:1243840206722044014>  - Counter Strike 2 \n\
             <:dayz:1243840207892119572>  - DayZ \n\
-            <:rocket_league:1243840202489991168>  - Rocket League", color=0xFF0080)
+            <:rocket_league:1243840202489991168>  - Rocket League \n\
+            <:minecraft:1243876361941160026>  - Minecraft \n\
+            <:JAVR_SMP:1243883118826622997>  - JAVR SMP", color=0xFF0080)
         if interaction.channel_id == 1243588381472985179:
             await interaction.response.send_message(embed=emb, ephemeral = False)
             for id in self.role_info.values():

59ac9eb957cf7e292e6c74ddd14d2fac2af08488
Arisen <pawelherzyk2002@gmail.com>
2024-05-30 20:10:19 +0200

    Play function refector, Added clear queue function, Discovered skip function not working, Started work on queue display function

--- cogs/music_yt.py
+++ cogs/music_yt.py
@@ -12,17 +12,15 @@ class Music_YT(commands.Cog):
         bot.tree.add_command(self.skip,guild=discord.Object(id=692802312720089108))
         bot.tree.add_command(self.pause,guild=discord.Object(id=692802312720089108))
         bot.tree.add_command(self.resume,guild=discord.Object(id=692802312720089108))
+        bot.tree.add_command(self.clear,guild=discord.Object(id=692802312720089108))
+        bot.tree.add_command(self.queue,guild=discord.Object(id=692802312720089108))
 
 
     @discord.app_commands.command(name="play", description="Add track to queue")
-    async def play(self,interaction: discord.Interaction, youtube: str = None, spotify: str = None):
+    async def play(self,interaction: discord.Interaction, youtube: str = None):#, spotify: str = None):
         if youtube != None:
             query: wavelink.Search = await wavelink.Playable.search(youtube)
-        if spotify:
-            await interaction.response.send_message("Spotify is not yet supported")
-        #print(type(query))
-        #print("------------")
-        #print(query[0])
+            
         if interaction.user.voice.channel:
             if not interaction.guild.voice_client:
                 vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player, self_deaf=True)
@@ -32,29 +30,23 @@ class Music_YT(commands.Cog):
                 await vc.move_to(interaction.user.voice.channel)
             else:
                 vc: wavelink.Player = interaction.client.voice_clients[0]
-            
-            
-            if type(query) == list:    
-                if vc.playing:
-                    await vc.queue.put(query[0])
-                else:
-                    await vc.play(track=query[0])
-                    await interaction.response.send_message(f"Now playing {track[0]}", ephemeral=True)
-            elif type(query) == wavelink.tracks.Playlist:
-                if vc.playing:
-                    for track in query:
-                       await vc.queue.put(track)
-                else:
-                    await vc.play(query[0])
-                    for i in range(1,len(query)):
-                        await vc.queue.put(query[i])
-                    
+                
+            if isinstance(query, wavelink.tracks.Playlist):
+                added: int = await vc.queue.put_wait(query)
             else:
-                if vc.playing:
-                    await vc.queue.put(query)
+                track: wavelink.Playable = query[0]
+                await vc.queue.put_wait(track)
+                added = 1
+                
+            if not vc.playing:
+                if vc.queue.is_empty:
+                    to_play = track
                 else:
-                    await vc.play(track=query)
-                    await interaction.response.send_message(f"Now playing {query[0]}", ephemeral=True)
+                    to_play = vc.queue.get()
+                await vc.play(to_play)
+                await interaction.response.send_message(f"Now playing {to_play}", ephemeral=True)
+            else:
+                await interaction.response.send_message(f"Added {added} tracks to the queue")
         else:
             await interaction.response.send_message("You are not in a voice channel", ephemeral=True)
             
@@ -74,7 +66,17 @@ class Music_YT(commands.Cog):
             await vc.stop()
             await interaction.response.send_message("Stopped the music",ephemeral=True)
         else:
-            await interaction.response.send_message("You are not in a voice channel", ephemeral=True)
+            await interaction.response.send_message("You are not in my voice channel", ephemeral=True)
+            
+    @discord.app_commands.command(name="clear",description="Clear the queue")
+    async def clear(self, interaction: discord.Interaction):
+        if interaction.user.voice.channel == interaction.client.voice_clients[0].channel:
+            vc: wavelink.Player = interaction.client.voice_clients[0]
+            if not vc.queue.is_empty:
+                await vc.queue.clear()
+            await interaction.response.send_message("Queue has been cleared")
+        else:
+            await interaction.response.send_message("You are not in my voice channel", ephemeral=True)
         
     @discord.app_commands.command(name="skip", description="Skip current track")
     async def skip(self, interaction: discord.Interaction):
@@ -110,20 +112,13 @@ class Music_YT(commands.Cog):
         else:
             await interaction.response.send_message("You are not in a voice channel", ephemeral=True)
             
-    #@commands.Cog.listener()
-    #async def on_wavelink_track_start(self, payload: wavelink.TrackStartEventPayload):
-    
-    @commands.Cog.listener()
-    async def on_wavelink_track_end(self, payload: wavelink.TrackEndEventPayload):
-        if not payload.player.queue.is_empty:
-            track = payload.player.queue.get()
-            await payload.player.play(track)
+    @discord.app_commands.command(name="queue",description="Display queue content")
+    async def queue(self, interaction: discord.Interaction):
+        if interaction.user.voice.channel == interaction.client.voice_clients[0].channel:
+            vc: wavelink.Player = interaction.client.voice_clients[0]
+            await interaction.response.send_message(f"Your queue is: {vc.queue}", ephemeral=True)
+        else:
+            await interaction.response.send_message("You are not in a voice channel", ephemeral=True)
 
-        
-        
-        
-    
-        
-        
 async def setup(bot):
     await bot.add_cog(Music_YT(bot))
